89523d5b5577e5da461653dc2909af6228bf03a8
Raul <rpelaez@linkcare.es>
2013-05-16 12:11:41 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MiPrimeraWebRuby::Application.config.secret_token = '7c0b1fe18ea8434dfbbc1208614bf07f240b82d9a8054bdb7fb3b70edafa19c84f2cb02b5d75105742173ce764d4f413843d07bb032641ad0281a129c8bc82a4'
